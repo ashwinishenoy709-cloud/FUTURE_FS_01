c2d3d24d7e2b1be7560389ee63ca619eebf1a449
ashwinishenoy709-cloud <ashwinishenoy709@gmail.com>
2026-09-11 16:47:13 +0530

    feat: Changed the icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,7 +97,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&family=Figtree:wght@400;500;600&display=swap",
       },
-      { rel: "icon", href: "/ashwini-logo2.png", type: "image/png" },
+      { rel: "icon", href: "/ashwini-logo23.png", type: "image/png" },
     ],
   }),
   shellComponent: RootShell,
